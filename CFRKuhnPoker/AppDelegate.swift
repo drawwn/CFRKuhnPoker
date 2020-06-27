d8ef97877b952ac18fbfc7ecf10b338f319dcb88
Daniel McLean <dmclean93@gmail.com>
2020-06-27 17:05:45 -0400

    Initial Commit

---
+++ CFRKuhnPoker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CFRKuhnPoker
+//
+//  Created by Daniel McLean on 6/27/20.
+//  Copyright © 2020 Daniel McLean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CFRKuhnPoker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CFRKuhnPoker
+//
+//  Created by Daniel McLean on 6/27/20.
+//  Copyright © 2020 Daniel McLean. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
